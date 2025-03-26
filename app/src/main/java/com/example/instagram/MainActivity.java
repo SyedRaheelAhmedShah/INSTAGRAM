bace93d761d2f2b59f4bd136413f251f6d90d089
Syed Raheel Ahmed Shah <syedraheel798@gmail.com>
2025-03-26 21:32:46 +0500

    Initial commit

---
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.instagram;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.NullCipher;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    toolbar=findViewById(R.id.homeToolbar);
+    setActionBar(toolbar);
+
+    if (getActionBar()!=null){
+//        getActionBar().setDisplayHomeAsUpEnabled(true);       for back button on toolbar 
+        getActionBar().setTitle("Instagram Clone");
+    }
+    }
+}
